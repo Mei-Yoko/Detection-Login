0aeef0aad9216156a01c05e4395a36bb616a8f5e
Mei-Yoko <keypongphob@gmail.com>
2026-01-26 11:49:05 +0700

    >

--- src/types/index.ts
+++ src/types/index.ts
@@ -21,5 +21,24 @@ resetLoginAttempts(): Promise<void>;
 }
 
 export interface ILoginAttempts extends Document{
+    email: string;
+    ipAddress: string;
+    userAgent: string;
+    success: boolean;
+    failureReson?: string;
+    timeStamp: Date;
+    location?: { country?: string; city?: string; };   
+}
+
+export interface IBlockIP extends Document{
+    ipAddress: string;
+    reason: string;
+    blockedAt: Date;
+    blockedUntil: Date;
+    attempCount: number;
+    isActive: boolean;
+}
+
+export enum SecurityEventType{
     
 }
