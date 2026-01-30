9b2e3ecc96b5e6c4b383e0bc7930419b7cdab7f1
Mei-Yoko <keypongphob@gmail.com>
2026-01-30 18:20:25 +0700

    :(

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,27 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async (): Promise<void> => {
     try{
+        // ดึง MongoDB URI
         const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost:27017/secure_login_db';
+        // เชื่อมต่อกับ MongoDB
         const conn = await mongoose.connect(mongoUri);
+
+        console.log(`MongoDB Connect: ${conn.connection.host}`);
+        console.log(`Database: ${conn.connection.name}`);
+
+        mongoose.connection.on('error', (err) => {
+            console.log('Mongo Connection Error', err);
+        });
+
+        mongoose.connection.on('disconnected',() => {
+            console.log('Mongo Disconected');
+        });
+
+        process.on('SIGINT', async () =>{
+            await mongoose.connection.close();
+            console.log('Mongo Shutdown by terminal');
+            process.exit(0);
+        });
+
     }
 }
