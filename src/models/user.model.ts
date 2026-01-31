7affb9086d3009b9a0d7fae1c5a2a08bafb73403
Mei-Yoko <keypongphob@gmail.com>
2026-01-31 11:03:23 +0700

    .

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,2 +1,28 @@
 import  mongoose,{Schema}  from "mongoose";
 import bcrypt from 'bcryptjs';
+import { Iuser } from "../types";
+
+const UserSchema = new Schema<Iuser>({
+    
+    email: {
+        type: String,required:true,
+        unique: true,lowercas:true,trim:true, 
+        match:[/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,'Please provide a valid email']
+    },
+
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength:[5,'Username must be least 5'],
+        maxlength:[30,'Username  max exceed 30']
+    },
+
+    password:{
+        type: String,
+        requried: true,
+        minlength:[12,'Password must least 12'],
+        select: false
+    },
+})
