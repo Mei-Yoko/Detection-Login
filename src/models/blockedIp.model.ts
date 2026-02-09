5abb4625e4d3c2565de77d28615b9d4b1cf7d571
Mei-Yoko <keypongphob@gmail.com>
2026-02-09 22:01:02 +0700

    Monday

--- src/models/blockedIp.model.ts
+++ src/models/blockedIp.model.ts
@@ -1,21 +1,49 @@
-import mongoose from "mongoose";
-import { IBlockIP } from "../types";
+import mongoose, { Schema } from 'mongoose';
+import { IBlockIP } from '../types';
 
-//Block ip
-const BlockedIPSchema =  new Schema<IBlockIP>(
-{
-     // IP Address
-    ipAddress:{
-        type: String,
-        required: true,
-        unique: true
+const BlockedIPSchema = new Schema<IBlockIP>(
+  {
+    //Ip address have been block
+    ipAddress: {
+      type: String,
+      required: true,
+      unique: true
     },
-     // Reason
-    reason:{
-        type: String,
-        required: true,
-        default: 'Too many failed login attempts'
+    //reason
+    reason: {
+      type: String,
+      required: true,
+      default: 'Too many failed login attempts'
+    },
+    // time block
+    blockedAt: {
+      type: Date,
+      default: Date.now
+    },
+    //block till
+    blockedUntil: {
+      type: Date,
+      required: true
+    },
+    // n try to login
+    attempCount: {
+      type: Number,
+      default: 0
+    },
+    //(active/inactve)
+    isActive: {
+      type: Boolean,
+      default: true
     }
-
-
+  },
+  {
+    timestamps: true
+  }
 );
+
+export const BlockedIP = mongoose.model<IBlockIP>('BlockedIP',BlockedIPSchema);
+
+//index
+BlockedIPSchema.index({ipAddress: 1});
+BlockedIPSchema.index({isActive: 1, BlockedIP: 1});
+BlockedIPSchema.index({blockedUntil: 1});
