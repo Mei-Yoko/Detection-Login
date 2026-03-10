77843433f641b3462c0ac8bd8cb3b9e032590850
Mei-Yoko <keypongphob@gmail.com>
2026-03-10 21:59:16 +0700

    Log

--- src/utils/bruteforce.utils.ts
+++ src/utils/bruteforce.utils.ts
@@ -17,4 +17,28 @@ export const isIPBlocked = async (ipAddress: string): Promise<boolean> => {
       return false;
     }
   };
+
+  /**
+  * login attempt
+  * @param email - Email try to login
+  * @param ipAddress - IP address
+  * @param success - Login success or not
+  * @param userAgent - Browser/device info
+  * @param failureReason - reason for failed
+  */
+ export const recordLoginAttempt = async (
+   email: string,
+   ipAddress: string,
+   success: boolean,
+   userAgent?: string,
+   failureReson?: 'invalid_credentials' | 'account_locked' | 'account_inactive' | 'ip_blocked' | 'rate_limited'
+ ): Promise<void> => {
+   await LoginAttempt.create({
+     email,
+     ipAddress,
+     success,
+     userAgent,
+     failureReson,
+   });
+ };
   
