4f83befb1bb3a4a2db68b6007c1082b90099cac6
Mei-Yoko <keypongphob@gmail.com>
2026-04-02 00:02:44 +0700

    utils

--- src/utils/bruteforce.utils.ts
+++ src/utils/bruteforce.utils.ts
@@ -1,7 +1,8 @@
 import{LoginAttempt} from '../models/LoginAttempt.model';
-import { BlockedIP, blockedIP } from '../models/BlockedIP.model';
+import { BlockedIP } from '../models/BlockedIP.model';
 import { SecurityLog } from '../models/SecurityLog.model';
 import { SecurityEventType } from '../types';
+
 
 /**
  * check for Ip is blocked or not
