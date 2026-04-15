521155d157f43b022847055676290802d9e751ec
Mei-Yoko <keypongphob@gmail.com>
2026-04-15 23:12:27 +0700

    main

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+//Main Application Entry point
+import  express ,{ Application, Request, Response  } from 'express';
+import cors form 'cors';
+import helmet from 'helmet';
+import dotenv form 'dotenv';
+import { connectDatabase } from './config/database';
+
+const app: Application = express();
+const port = process.env.PORT || 5000;
